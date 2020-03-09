a078d434742e0d09f06df779886207d9cb945af6
Marllon Panisset <marllonop@gmail.com>
2020-03-09 18:08:04 -0300

    Começando curso react ninja

---
+++ examples/react-webpack/src/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import Title from './app'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render (
+    React.createElement(Title),
+    document.querySelector('[data-js="app"]')
+)
